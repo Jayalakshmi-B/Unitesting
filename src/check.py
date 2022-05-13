a9569d909471c8b3f9f0a156c60bb902734e19da
Jayalakshmi-B <70580391+Jayalakshmi-B@users.noreply.github.com>
2022-05-13 11:16:41 +0530

    Update check.py

--- src/check.py
+++ src/check.py
@@ -1,18 +1,21 @@
-#importing required packages
+# importing required packages
 import json
+import boto3
 
-#Creating lambda handler function
+
+# Creating lambda handler function
 def lambda_handler(event, ):
     print(event)
     numbers = event['numbers']
-    response = json.dumps({"Armstrong":armstrong(numbers[0],numbers[1])})  #converting python to json
+    response = json.dumps({"Armstrong": armstrong(numbers[0], numbers[1])})  # converting python to json
     return {
         "statusCode": 200,
         "body": response,
     }
 
-#Creating Armstrong Function
-def armstrong(numbers,sum):
+
+# Creating Armstrong Function
+def armstrong(numbers, sum):
     try:
         temp = numbers
         if numbers == 0:
@@ -27,6 +30,20 @@ def armstrong(numbers,sum):
                 return "Passed"
             else:
                 raise ValueError
-#throws error when the given number is not an armstrong
+    # throws error when the given number is not an armstrong
     except ValueError:
+        return "Error"
+
+
+def employee():
+    try:
+        dynamodb = boto3.resource('dynamodb')
+        table = dynamodb.Table('Employee')
+        response = table.get_item(
+            Key={
+                'id': 1
+            }
+        )
+        return response['Item']
+    except Exception:
         return "Error"
